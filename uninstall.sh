ce8b831de58fe90f5e9ff418081b7d5421b71fc2
𝕊ℙ𝕆𝕋𝕐 𝕄𝕋𝔽 <blaisedave2006@gmail.com>
2025-08-02 18:13:32 -0400

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -2,15 +2,11 @@
 
 echo "[*] Désinstallation de Termux Password Protector"
 
-# Restaurer le shell original
 if [ -f /data/data/com.termux/files/usr/bin/login.real ]; then
-    echo "[*] Restauration du shell original..."
     rm -f /data/data/com.termux/files/usr/bin/login
     mv /data/data/com.termux/files/usr/bin/login.real /data/data/com.termux/files/usr/bin/login
 fi
 
-# Supprimer les fichiers
-echo "[*] Suppression des fichiers..."
 rm -f /data/data/com.termux/files/usr/bin/termux-auth
 rm -f /data/data/com.termux/files/usr/etc/termux_password.hash
 rm -f /data/data/com.termux/files/usr/etc/termux_lock
